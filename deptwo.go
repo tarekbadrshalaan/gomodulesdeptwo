9197a40cbb265e1acccca26c9905cefb6436c0c5
tarekbadr <tarekbadrshalaan@gmail.com>
2018-10-02 16:00:21 +0200

    v1.0.0

--- deptwo.go
+++ deptwo.go
@@ -1,1 +1,12 @@
 package gomodulesdeptwo
+
+import (
+	"github.com/tarekbadrshalaan/gomodulesdepone/v2/subpkg"
+)
+
+// GetDatadepone : get data from depone
+func GetDatadepone() string {
+
+	return subpkg.GetExtraData()
+}
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tarekbadrshalaan/gomodulesdeptwo
+
+require github.com/tarekbadrshalaan/gomodulesdepone/v2 v2.0.0
